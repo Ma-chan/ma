2bbc9bfebe43214ed8dc314f50e37178e4e4e619
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2017-12-01 22:08:24 +0900

    Add files via upload

---
+++ ホームページ作成.js
@@ -0,0 +1,41 @@
+
+(function($){ 
+
+
+    $(function(){ 
+
+
+  
+
+
+        $('#loopSlide ul').simplyScroll({ 
+
+
+            autoMode: 'loop', 
+
+
+            speed: 1, 
+
+
+            frameRate: 24, 
+
+
+            horizontal: true, 
+
+
+            pauseOnHover:   true, 
+
+
+            pauseOnTouch: true 
+
+
+        }); 
+
+
+          
+
+
+    });  
+
+
+})(jQuery); 
